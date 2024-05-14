44fa939c69d60592921208d1405f7b97767300f6
Arpan Sethiya <arpansethiya2014@gmail.com>
2024-05-15 02:06:44 +0530

    Update RestApisApplication.java

--- src/main/java/com/arp/RestApisApplication.java
+++ src/main/java/com/arp/RestApisApplication.java
@@ -5,10 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-
+	
 @SpringBootApplication
 public class RestApisApplication extends SpringBootServletInitializer{
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(RestApisApplication.class, args);
 	}
