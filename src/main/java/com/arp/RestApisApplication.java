e2a1e8116474cc45bee1a9abfe405dd0f6997f41
Arpan Sethiya <arpansethiya2014@gmail.com>
2024-05-15 02:03:53 +0530

    Changes in pom.xml file

--- src/main/java/com/arp/RestApisApplication.java
+++ src/main/java/com/arp/RestApisApplication.java
@@ -2,13 +2,22 @@ package com.arp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 
 @SpringBootApplication
-public class RestApisApplication {
+public class RestApisApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestApisApplication.class, args);
 	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(RestApisApplication.class);
+	}
+	
+	
 
 }
--- src/main/java/com/arp/controller/StudentController.java
+++ src/main/java/com/arp/controller/StudentController.java
@@ -5,10 +5,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-//@RequestMapping("/api")
+@RequestMapping("/api")
 public class StudentController {
 
-	@GetMapping("/")
+	@GetMapping("/message")
 	public String message() {
 		return "Hi Arpan...!";
 	}
